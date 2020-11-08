557d934dca37568ed579739606128476a7de0009
alliecookie <allison.gaffiero@outlook.fr>
2020-11-08 20:01:03 +0100

    Open Classeroom - applications livres

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
 import { Component } from '@angular/core';
+// import core firebase client (required)
+import firebase from '@firebase/app';
+// import Firebase Realtime Database (optional)
+import '@firebase/database';
+// import Cloud Firestore (optional)
+import '@firebase/firestore';
+// import Firebase Authentication (optional)
+import '@firebase/auth';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ocBooks';
+  constructor(){
+    const config = {
+      apiKey: "AIzaSyDkaBJ_29CsboVA_KaTpuZvOa8AgiCJ9HQ",
+      authDomain: "ocbooks-16f63.firebaseapp.com",
+      databaseURL: "https://ocbooks-16f63.firebaseio.com",
+      projectId: "ocbooks-16f63",
+      storageBucket: "ocbooks-16f63.appspot.com",
+      messagingSenderId: "487125831997",
+      appId: "1:487125831997:web:bc9929213ce3bc4308b554",
+      measurementId: "G-SWPB818P6Y"
+    };
+    firebase.initializeApp(config);
+//    firebase.analytics();
+
+  }
 }
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+// import core firebase client (required)
+import firebase from '@firebase/app';
+// import Firebase Authentication (optional)
+import '@firebase/auth';
+
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+  createNewUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().createUserWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signInUser(email: string, password: string) {
+    return new Promise(
+      (resolve, reject) => {
+        firebase.auth().signInWithEmailAndPassword(email, password).then(
+          () => {
+            resolve();
+          },
+          (error) => {
+            reject(error);
+          }
+        );
+      }
+    );
+  }
+
+  signOutUser() {
+    firebase.auth().signOut();
+  }
+
+}
